8a3efb50e1b2d63d2443d9977f3b3ebe078e33b8
McCray <tubach75@hotmail.com>
2021-05-25 09:07:09 -0400

    commit tree 55d8da5cdb21f4bb83c18fd6c5c97320c3ce3fe3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, bday){
+    const array = []; {
+    for(let i = 0; i < names.length; i++){
+      array.push(`Thank you, ${names[i]}, for the wonderful ${bday} gift!`);
+      }
+    }
+    return array;
+  }
+  
+  console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+  function countDown(number){
+    while(number >= 0){
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(10)
